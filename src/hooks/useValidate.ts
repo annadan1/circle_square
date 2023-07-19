823313cd6059168c3f3b07090c6f1be0820fac50
annadan1 <anna_s92_@mail.ru>
2023-07-19 21:14:19 +0400

    Add hook for validation

--- src/hooks/useValidate.ts
+++ src/hooks/useValidate.ts
@@ -1,9 +1,31 @@
-const rules = {
-    form: ['circle', 'square'],
-    color: ['red', 'green', 'blue', 'yellow'],
-    dark: [true, false],
+interface Rules {
+  [key: string]: string[] | boolean[];
+  form: string[];
+  color: string[];
+  dark: boolean[] | string[];
 }
 
-export const useValidate = () => {
-
+interface Item {
+  form: string;
+  color: string;
+  dark: boolean;
 }
+
+type ExpectedTypes = {
+  [K in keyof Rules]: K extends keyof Item ? Item[K] : never;
+};
+
+const rules: Rules = {
+  form: ["circle", "square"],
+  color: ["red", "green", "blue", "yellow"],
+  dark: [true, false],
+};
+
+export const useValidate = (item: ExpectedTypes): boolean => {
+  for (let rule in rules) {
+    const key = rule as keyof Rules;
+    if (!item.hasOwnProperty(key)) return false;
+    if (!rules[key].includes(item[key])) return false;
+  }
+  return true;
+};
